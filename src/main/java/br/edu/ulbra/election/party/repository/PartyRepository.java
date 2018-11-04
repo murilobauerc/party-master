3892c9625677a29d0983620cd8d4afc6813fc07f
murilobauerc <panchobauer@gmail.com>
2018-11-04 18:22:24 -0200

    release v0.1.0 set to go

---
+++ src/main/java/br/edu/ulbra/election/party/repository/PartyRepository.java
@@ -0,0 +1,11 @@
+package br.edu.ulbra.election.party.repository;
+
+import br.edu.ulbra.election.party.model.Party;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PartyRepository extends CrudRepository<Party, Long> {
+}
+
+
+
+
